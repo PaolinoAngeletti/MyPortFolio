be471efc89e4bef2d82bdddab2bb8c3b1214958e
Paolino Angeletti <ang.paolino@alice.it>
2025-10-02 16:12:27 +0200

    build project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+
+    // build save path
+    outDir: '.',
+
+    // avoid to delete my files in root before build
+    emptyOutDir: false,
+  },
 })
