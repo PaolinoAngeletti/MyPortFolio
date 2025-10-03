38c878abbb0491f0e9a341db63f260931a4da9be
Paolino Angeletti <ang.paolino@alice.it>
2025-10-03 16:17:47 +0200

    update link to cv

--- src/components/sections/Download.jsx
+++ src/components/sections/Download.jsx
@@ -3,8 +3,8 @@ export default function Home() {
         <div>
             <h1>Curriculum vitae</h1>
             <p>Usa questa sezione per eseguire il download del mio Curriculum Vitae &#128521;</p>
-            <a href="/AngelettiPaolino_CV_IT.pdf" download>
-                Scarica il mio CV
+            <a href="https://paolinoangeletti.github.io/MyPortFolio/AngelettiPaolino_CV_IT.pdf" target="_blank">
+                Apri CV
             </a>
         </div>
     );
