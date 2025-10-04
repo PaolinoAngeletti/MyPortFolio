e43c01f4cff79c121aa69a40ab7d18b018da7868
Paolino Angeletti <ang.paolino@alice.it>
2025-10-04 17:37:12 +0200

    added technologies to home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,50 @@
 import { useState, useEffect } from "react";
-import Sidebar from "./components/Sidebar";
-import DetailView from "./components/DetailView";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Sidebar from "./components/main/Sidebar";
+import DetailView from "./components/main/DetailView";
+
+import Home from "./components/sections/Home";
+import Technologies from "./components/sections/Technologies";
+
 
 function App() {
   
   const [selected, setSelected] = useState("Home");
   const [sidebarOpen, setSidebarOpen] = useState(() => window.innerWidth >= 768);
+
+  const [targetParagraph, setTargetParagraph] = useState(null);
+
+  const handleCardClick = (section, paragraphId) => {
+  setSelected(section);        // esempio: "Technologies"
+  setTargetParagraph(paragraphId); // esempio: "Paragrafo1"
+};
+
+  console.log("Sono in APP con section " + selected + "--" + setSelected);
+
+  // return (
+  //   <Router>
+  //     <div className="app-container">
+  //       <button
+  //         className="hamburger"
+  //         onClick={() => setSidebarOpen(!sidebarOpen)}
+  //       >
+  //         ☰
+  //       </button>
+
+  //       <Sidebar selected={selected} setSelected={setSelected} open={sidebarOpen} />
+
+  //       <div className="detail">
+  //         {/* Qui usiamo Routes solo per la parte “detail view” */}
+  //         <Routes>
+  //           <Route path="/" element={<Home />} />
+  //           <Route path="/technologies" element={<Technologies />} />
+  //           {/* altre pagine qui */}
+  //         </Routes>
+  //       </div>
+  //     </div>
+  //   </Router>
+  // );
 
   return (
     <div className="app-container">
@@ -20,7 +59,7 @@ function App() {
       <Sidebar selected={selected} setSelected={setSelected} open={sidebarOpen} />
       
       <div className="detail">
-        <DetailView section={selected} />
+        <DetailView section={selected} handleCardClick={handleCardClick} />
       </div>
 
     </div>
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,6 +1,11 @@
 import styles from './Home.module.css';
+import { retrieveGroupedByType } from '../../repository/technologiesRepository';
+import Card from '../graphic/Card';
 
-export default function Home() {
+export default function Home({handleCardClick}) {
+
+    const technologiesList = retrieveGroupedByType();
+
     return (
         <div>
             <h1>Paolino Angeletti</h1>
@@ -11,38 +16,38 @@ export default function Home() {
 
             <h2>Informazioni personali</h2>
 
-            <div class={styles.detail_title}>
+            <div className={styles.detail_title}>
                 <h3>🎂 Data di nascita</h3>
-                <p class={styles.information}>04 Maggio 1993</p>
+                <p className={styles.information}>04 Maggio 1993</p>
             </div>
 
-            <div class={styles.detail_title}>
+            <div className={styles.detail_title}>
                 <h3>🌍 Nazionalità</h3>
-                <p class={styles.information}>Italiana</p>
+                <p className={styles.information}>Italiana</p>
             </div>
 
-            <div class={styles.detail_title}>
+            <div className={styles.detail_title}>
                 <h3>💬 Livello Inglese</h3>
-                <p class={styles.information}>B1</p>
+                <p className={styles.information}>B1</p>
             </div>
 
             <h2>Istruzione</h2>
 
-            <div class={styles.detail_title}>
+            <div className={styles.detail_title}>
                 <h3>Laurea Triennale in Informatica</h3>
-                <p class={styles.information}>Università Federico II, Napoli • 2012 - 2016 • Votazione: 100/110</p>
+                <p className={styles.information}>Università Federico II, Napoli • 2012 - 2016 • Votazione: 100/110</p>
             </div>
 
-            <div class={styles.detail_title}>
+            <div className={styles.detail_title}>
                 <h3>Diploma Perito Commerciale e Programmatore</h3>
-                <p class={styles.information}>ITCG Masullo-Theti, Nola (NA) • 2009 - 2012 • Votazione: 74/100</p>
+                <p className={styles.information}>ITCG Masullo-Theti, Nola (NA) • 2009 - 2012 • Votazione: 74/100</p>
             </div>
 
             <h2>Esperienze professionali</h2>
 
-            <div class={styles.detail_title}>
+            <div className={styles.detail_title}>
                 <h3>Hitachi Rail (Consultant @ Teoresi Group S.p.A.)</h3>
-                <p class={styles.information}>Napoli • Giugno 2024 - Ottobre 2025</p>
+                <p className={styles.information}>Napoli • Giugno 2024 - Ottobre 2025</p>
                 <ul>
                     <li>
                         Progetto: 360Motion
@@ -65,9 +70,9 @@ export default function Home() {
                 </ul>
             </div>
 
-            <div class={styles.detail_title}>
+            <div className={styles.detail_title}>
                 <h3>Hitachi Rail (Consultant @ Teoresi Group S.p.A.)</h3>
-                <p class={styles.information}>Napoli • Gennaio 2024 - Aprile 2024</p>
+                <p className={styles.information}>Napoli • Gennaio 2024 - Aprile 2024</p>
                 <ul>
                     <li>
                         Progetto: 360FlowVehicle
@@ -84,9 +89,9 @@ export default function Home() {
                 </ul>
             </div>
 
-            <div class={styles.detail_title}>
+            <div className={styles.detail_title}>
                 <h3>Advise S.R.L.</h3>
-                <p class={styles.information}>Mugnano del Cardinale (AV) • Giugno 2016 - Gennaio 2024</p>
+                <p className={styles.information}>Mugnano del Cardinale (AV) • Giugno 2016 - Gennaio 2024</p>
                 <ul>
                     <li>
                         Progetti Android (Java) & Back end (Node.js, Python)
@@ -106,9 +111,9 @@ export default function Home() {
                 </ul>
             </div>
 
-            <div class={styles.detail_title}>
+            <div className={styles.detail_title}>
                 <h3>Software Business S.R.L.</h3>
-                <p class={styles.information}>Mugnano del Cardinale (AV) • Tirocinio universitario • Marzo 2016 - Agosto 2016</p>
+                <p className={styles.information}>Mugnano del Cardinale (AV) • Tirocinio universitario • Marzo 2016 - Agosto 2016</p>
                 <ul>
                     <li>
                         Progettazione e rilascio piattaforma ordini B2B integrata SAP
@@ -117,6 +122,19 @@ export default function Home() {
             </div>
 
             <h2>Competenze tecniche</h2>
+
+            {Object.entries(technologiesList).map(([type, technologies]) => (
+                <div className={styles.detail_title} key={type}>
+                    <h3>{type}</h3>
+                    <div style={{ display: 'flex' }}>
+                        {technologies.map((technology) => (
+                            <Card
+                                key={technology.name}
+                                title={technology.name} onClick={() => handleCardClick("Tecnologie", "Java")}
+                            />
+                        ))}</div>
+                </div>
+            ))}
 
         </div>
     );
