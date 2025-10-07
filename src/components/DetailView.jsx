d6dc4fd0988a09bc3538da950b4b2803c349631a
Paolino Angeletti <ang.paolino@alice.it>
2025-10-07 14:41:43 +0200

    fixed routes without router

--- src/components/DetailView.jsx
+++ src/components/DetailView.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "../pages/Home";
 import Download from "../pages/Download";
@@ -8,12 +8,14 @@ import Technologies from "../pages/Technologies";
 
 export default function DetailView() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/companies" element={<Companies />} />
-      <Route path="/technologies" element={<Technologies />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/download" element={<Download />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/companies" element={<Companies />} />
+        <Route path="/technologies" element={<Technologies />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/download" element={<Download />} />
+      </Routes>
+    </Router>
   );
 }
