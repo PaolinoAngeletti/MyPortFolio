392ddaf99940fde1058db913d31bcd8a680f9d4b
Paolino Angeletti <ang.paolino@alice.it>
2025-10-07 11:23:26 +0200

    added companies and projects pages, empty actually

---
+++ src/components/DetailView.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Download from "../pages/Download";
+import Projects from "../pages/Projects";
+import Companies from "../pages/Companies";
+import Technologies from "../pages/Technologies";
+
+export default function DetailView() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/companies" element={<Companies />} />
+      <Route path="/technologies" element={<Technologies />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/download" element={<Download />} />
+    </Routes>
+  );
+}
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,7 @@
+export default function Projects() {
+    return (
+        <div>
+            <h1>Progetti</h1>
+        </div>
+    );
+}
